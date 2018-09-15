495054bb54103d1d70b0b086cf691b2ce5371b6f
alex <qwert2603@mail.ru>
2018-09-15 22:21:09 +0700

    * rename strings resources

--- app/src/main/java/com/qwert2603/crmit_android/details_fragments/StudentDetailsFragment.kt
+++ app/src/main/java/com/qwert2603/crmit_android/details_fragments/StudentDetailsFragment.kt
@@ -40,20 +40,20 @@ class StudentDetailsFragment : EntityDetailsFragment<StudentFull>() {
 
     override fun StudentFull.toDetailsList(): List<EntityDetailsListItem> = listOfNotNull(
             EntityDetailsStudentSystemInfo(systemUser.enabled, filled),
-            EntityDetailsField(R.string.student_details_field_login, systemUser.login, R.drawable.ic_person_black_24dp),
-            EntityDetailsField(R.string.student_details_field_lessonsAttendedCount, lessonsAttendedCount.toString()),
-            EntityDetailsField(R.string.student_details_field_birthDate, showingBirthDate()),
-            EntityDetailsField(R.string.student_details_field_birthPlace, birthPlace),
-            EntityDetailsField(R.string.student_details_field_registrationPlace, registrationPlace),
-            EntityDetailsField(R.string.student_details_field_actualAddress, actualAddress),
-            additionalInfo?.takeIf { it.isNotEmpty() }?.let { EntityDetailsField(R.string.student_details_field_additionalInfo, it) },
-            knownFrom?.takeIf { it.isNotEmpty() }?.let { EntityDetailsField(R.string.student_details_field_knownFrom, it) },
-            EntityDetailsField(R.string.student_details_field_school, getString(R.string.student_school_format, school.name, grade, shift)),
-            phone?.let { EntityDetailsField(R.string.student_details_field_phone, it, R.drawable.ic_local_phone_black_24dp) },
-            EntityDetailsField(R.string.student_details_field_contactPhone, "${contactPhoneNumber} (${contactPhoneWho})", R.drawable.ic_local_phone_black_24dp),
-            EntityDetailsField(R.string.student_details_field_citizenshipName, citizenshipName),
-            mother?.let { EntityDetailsField(R.string.student_details_field_mother, it.toTextFieldValue()) },
-            father?.let { EntityDetailsField(R.string.student_details_field_father, it.toTextFieldValue()) },
+            EntityDetailsField(R.string.detailsField_login, systemUser.login, R.drawable.ic_person_black_24dp),
+            EntityDetailsField(R.string.detailsField_lessonsAttendedCount, lessonsAttendedCount.toString()),
+            EntityDetailsField(R.string.detailsField_birthDate, showingBirthDate()),
+            EntityDetailsField(R.string.detailsField_birthPlace, birthPlace),
+            EntityDetailsField(R.string.detailsField_registrationPlace, registrationPlace),
+            EntityDetailsField(R.string.detailsField_actualAddress, actualAddress),
+            additionalInfo?.takeIf { it.isNotEmpty() }?.let { EntityDetailsField(R.string.detailsField_additionalInfo, it) },
+            knownFrom?.takeIf { it.isNotEmpty() }?.let { EntityDetailsField(R.string.detailsField_knownFrom, it) },
+            EntityDetailsField(R.string.detailsField_school, getString(R.string.student_school_format, school.name, grade, shift)),
+            phone?.let { EntityDetailsField(R.string.detailsField_phone, it, R.drawable.ic_local_phone_black_24dp) },
+            EntityDetailsField(R.string.detailsField_contactPhone, "${contactPhoneNumber} (${contactPhoneWho})", R.drawable.ic_local_phone_black_24dp),
+            EntityDetailsField(R.string.detailsField_citizenshipName, citizenshipName),
+            mother?.let { EntityDetailsField(R.string.detailsField_mother, it.toTextFieldValue()) },
+            father?.let { EntityDetailsField(R.string.detailsField_father, it.toTextFieldValue()) },
             EntityDetailsGroupsList(groups)
     )
 
@@ -75,13 +75,13 @@ class StudentDetailsFragment : EntityDetailsFragment<StudentFull>() {
     }
 
     private fun Parent.toTextFieldValue() = listOf(
-            R.string.student_details_field_fio to fio,
-            R.string.student_details_field_phone to phone,
-            R.string.student_details_field_address to address,
-            R.string.student_details_field_email to email,
-            R.string.student_details_field_vkLink to vkLink,
-            R.string.student_details_field_homePhone to homePhone,
-            R.string.student_details_field_notificationTypesString to notificationTypesString.takeIf { it.isNotEmpty() }?.reduce { acc, s -> "$acc / $s" }
+            R.string.detailsField_fio to fio,
+            R.string.detailsField_phone to phone,
+            R.string.detailsField_address to address,
+            R.string.detailsField_email to email,
+            R.string.detailsField_vkLink to vkLink,
+            R.string.detailsField_homePhone to homePhone,
+            R.string.detailsField_notificationTypesString to notificationTypesString.takeIf { it.isNotEmpty() }?.reduce { acc, s -> "$acc / $s" }
     )
             .filter { it.second != null }
             .map { "${getString(it.first)}: ${it.second}" }
