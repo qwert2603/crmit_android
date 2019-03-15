687d1a90aa74e477f8310d09fb41fff6c4aef8bd
alex <qwert2603@mail.ru>
2019-03-15 18:40:29 +0700

    color accent text/icon in selected drawer item

--- app/src/main/java/com/qwert2603/crmit_android/navigation/NavigationItemViewHolder.kt
+++ app/src/main/java/com/qwert2603/crmit_android/navigation/NavigationItemViewHolder.kt
@@ -1,8 +1,10 @@
 package com.qwert2603.crmit_android.navigation
 
+import android.graphics.PorterDuff
 import android.view.View
 import android.view.ViewGroup
 import com.qwert2603.andrlib.base.recyclerview.BaseRecyclerViewHolder
+import com.qwert2603.andrlib.util.color
 import com.qwert2603.crmit_android.R
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_navigation_menu.view.*
@@ -15,7 +17,11 @@ class NavigationItemViewHolder(parent: ViewGroup) : BaseRecyclerViewHolder<Navig
         super.bind(m)
         icon_ImageView.setImageResource(m.iconRes)
         title_TextView.setText(m.titleRes)
-        itemView.isSelected = m.screen == (adapter as? NavigationAdapter)?.selectedScreen
+        val selected = m.screen == (adapter as? NavigationAdapter)?.selectedScreen
+        itemView.isSelected = selected
+        val tintColor = resources.color(if (selected) R.color.colorAccent else android.R.color.black)
+        title_TextView.setTextColor(tintColor)
+        icon_ImageView.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
     }
 
 }
