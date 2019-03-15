6cc13f2954c39cdb7a19ab1bc226a547a066f16c
alex <qwert2603@mail.ru>
2019-03-15 11:20:55 +0700

    update drawer items design

--- app/src/main/java/com/qwert2603/crmit_android/navigation/NavigationItemViewHolder.kt
+++ app/src/main/java/com/qwert2603/crmit_android/navigation/NavigationItemViewHolder.kt
@@ -1,10 +1,8 @@
 package com.qwert2603.crmit_android.navigation
 
-import android.graphics.PorterDuff
 import android.view.View
 import android.view.ViewGroup
 import com.qwert2603.andrlib.base.recyclerview.BaseRecyclerViewHolder
-import com.qwert2603.andrlib.util.color
 import com.qwert2603.crmit_android.R
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_navigation_menu.view.*
@@ -17,13 +15,7 @@ class NavigationItemViewHolder(parent: ViewGroup) : BaseRecyclerViewHolder<Navig
         super.bind(m)
         icon_ImageView.setImageResource(m.iconRes)
         title_TextView.setText(m.titleRes)
-        val selected = m.screen == (adapter as? NavigationAdapter)?.selectedScreen
-        itemView.isSelected = selected
-        with(itemView) {
-            val tintColor = resources.color(if (selected) R.color.colorAccent else android.R.color.black)
-            title_TextView.setTextColor(tintColor)
-            icon_ImageView.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
-        }
+        itemView.isSelected = m.screen == (adapter as? NavigationAdapter)?.selectedScreen
     }
 
 }
